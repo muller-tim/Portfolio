47b6a4e6cd817e9a90366fb1c1a817c77a5beb4d
Tim Muller <muller.tim@outlook.com>
2025-10-07 20:19:21 +0200

    Changed Image Paths

--- js/includer.js
+++ js/includer.js
@@ -1,6 +1,6 @@
 async function loadPage(path) {
     const content = document.getElementById("content");
-    const page = path === "/Portfolio" ? "/pages/home.html" : path;
+    const page = path === "/Portfolio" || path === "/" ? "/pages/home.html" : path;
     
     try {
         const response = await fetch(`/Portfolio${page}`);
